5243e407116e49eeeab21928571ca695a464d88a
Dim <dcaramello@gmail.com>
2020-08-28 13:50:45 +0200

    statistique.html and js is functionnal

---
+++ js/statistiques.js
@@ -0,0 +1,34 @@
+// variables for the request object and to point to the html array
+let request = new XMLHttpRequest();
+let table = document.getElementById("table");
+
+// function that will create each element of the array with for loops
+request.onreadystatechange = function() {
+    
+    if (this.readyState === XMLHttpRequest.DONE && this.status === 200) {
+
+        let data = JSON.parse(this.response);
+            
+        let statistiques = data.statistiques;
+        console.log(statistiques);
+
+        // we create the line (tr) of the table with a for of
+        for (stat of statistiques) {
+            console.log(stat);
+            let tr = document.createElement("tr");
+                for(prop in stat) {
+                    let td = document.createElement("td");
+                    td.innerText = stat[prop];
+                    tr.appendChild(td);
+                }
+            table.appendChild(tr);              
+        }              
+    } 
+    else {       
+        console.log("une erreure est survenue");
+    }
+};
+
+request.open('GET', "statistiques.json", true);
+request.send();
+
